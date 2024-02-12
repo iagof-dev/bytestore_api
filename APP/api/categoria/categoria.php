ff42967c28378b09abc0df879220b75c9b6e8b8d
Iago "N3rdy" Fragnan <n3rdydzn@hotmail.com>
2024-02-12 11:51:24 -0300

    Delete APP directory

--- APP/api/categoria/categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-if ($api == 'categoria' && $method == 'GET') {
-    include_once("get.php");
-}
-if ($api == 'categoria' && $method == 'POST') {
-    include_once("post.php");
-}
--- APP/api/usuarios/usuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ($api == 'usuario' && $method == 'GET') {
-    include_once("get.php");
-}
-if ($api == 'usuario' && $method == 'POST') {
-    include_once("post.php");
-}
